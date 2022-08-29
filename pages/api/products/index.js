cb6f3613e3f5375540a688be23963fc5493a6e94
chidestech <dnwosu008@gmail.com>
2022-08-29 05:34:24 +0100

    new-mongodb-connection

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -3,18 +3,12 @@ import dbConnect from "../../../utils/MongoConnect";
 
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default async function handler(req, res) {
-    dbConnect();
+async function handler(req, res) {
+    
 
     if (req.method === "GET") {
         try {
-            // const products = await Product.find();
-            // res.status(200).send(products);
-          const  products = [
-                {_id : 1 ,name :"first", image :"http://res.cloudinary.com/chidestech/image/upload/v1641464145/awsvoz6xbck77htqe5qf.jpg", price:7000},
-                {_id : 2 ,name :"first", image :"http://res.cloudinary.com/chidestech/image/upload/v1641464145/awsvoz6xbck77htqe5qf.jpg", price:7000},
-                {_id : 3 ,name :"first", image :"http://res.cloudinary.com/chidestech/image/upload/v1641464145/awsvoz6xbck77htqe5qf.jpg", price:7000},
-            ]
+            const products = await Product.find();
             res.status(200).send(products);
 
         } catch (error) {
@@ -32,3 +26,6 @@ export default async function handler(req, res) {
         }
     }
 }
+
+
+export default dbConnect(handler);
--- utils/MongoConnect.js
+++ utils/MongoConnect.js
@@ -1,44 +1,66 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-// const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI
-// const MONGODB_URI = process.env.NEXT_PUBLIC_ATLAS_URI
+// // const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI
+// // const MONGODB_URI = process.env.NEXT_PUBLIC_ATLAS_URI
+// const MONGODB_URI = "mongodb://chidestech:1Luvumum%2f@cluster0-shard-00-00.pzpph.mongodb.net:27017,cluster0-shard-00-01.pzpph.mongodb.net:27017,cluster0-shard-00-02.pzpph.mongodb.net:27017/kenac-sports?ssl=true&replicaSet=atlas-sg448v-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+
+
+// if (!MONGODB_URI) {
+//   throw new Error(
+//     'Please define the MONGODB_URI environment variable inside .env.local'
+//   )
+// }
+
+// /**
+//  * Global is used here to maintain a cached connection across hot reloads
+//  * in development. This prevents connections growing exponentially
+//  * during API Route usage.
+//  */
+// let cached = global.mongoose
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null }
+// }
+
+// async function dbConnect() {
+//   if (cached.conn) {
+//     return cached.conn
+//   }
+
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     }
+
+//     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+//       return mongoose
+//     })
+//   }
+//   cached.conn = await cached.promise
+//   return cached.conn
+// }
+
+// export default dbConnect
+
+
+
+import mongoose from 'mongoose';
 const MONGODB_URI = "mongodb://chidestech:1Luvumum%2f@cluster0-shard-00-00.pzpph.mongodb.net:27017,cluster0-shard-00-01.pzpph.mongodb.net:27017,cluster0-shard-00-02.pzpph.mongodb.net:27017/kenac-sports?ssl=true&replicaSet=atlas-sg448v-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-
-
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
+const connectDB = handler => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
   }
+  // Use new db connection
+  await mongoose.connect(MONGODB_URI , {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true
+  });
+  return handler(req, res);
+};
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    }
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
-}
-
-export default dbConnect
+export default connectDB;
